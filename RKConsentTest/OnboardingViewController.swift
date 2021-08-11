8ce91ee35830a0844226409c4577f60ca6d27538
Vishnu Ravi <vishnu.ravi@gmail.com>
2021-08-11 10:59:55 -0400

    Add Completion Step + Clean up

--- RKConsentTest/OnboardingViewController.swift
+++ RKConsentTest/OnboardingViewController.swift
@@ -12,19 +12,18 @@ import ResearchKit
 
 struct OnboardingViewController: UIViewControllerRepresentable {
     
-    func makeCoordinator() -> OnboardingViewCoordinator {
-        OnboardingViewCoordinator()
-    }
-
     typealias UIViewControllerType = ORKTaskViewController
     
     func updateUIViewController(_ taskViewController: ORKTaskViewController, context: Context) {}
+    
     func makeUIViewController(context: Context) -> ORKTaskViewController {
 
+        /* **************************************************************
+        *  Get user consent
+        **************************************************************/
         let Document = ORKConsentDocument()
-        
         Document.title = "Test Consent"
-        print(Document)
+
         let sectionTypes: [ORKConsentSectionType] = [
             .overview,
             .dataGathering,
@@ -35,10 +34,11 @@ struct OnboardingViewController: UIViewControllerRepresentable {
             .studyTasks,
             .withdrawing
         ]
+        
         let consentSections: [ORKConsentSection] = sectionTypes.map { contentSectionType in
             let consentSection = ORKConsentSection(type: contentSectionType)
             consentSection.summary = "Complete the study"
-            consentSection.content = "This survey will ask you three questions and you will also measure your tapping speed by performing a small activity."
+            consentSection.content = "This is a test of the consent process."
             return consentSection
         }
         Document.sections = consentSections
@@ -47,7 +47,7 @@ struct OnboardingViewController: UIViewControllerRepresentable {
         let consentStep = ORKVisualConsentStep(identifier: "VisualConsentStep", document: Document)
         
         /* **************************************************************
-        *  STEP (2): ask user to review and sign consent document
+        *  Ask user to review and sign consent document
         **************************************************************/
         // use the `ORKConsentReviewStep` from ResearchKit
         let signature = Document.signatures?.first
@@ -56,35 +56,29 @@ struct OnboardingViewController: UIViewControllerRepresentable {
         reviewConsentStep.reasonForConsent = "Reason for Consent"
         
         /* **************************************************************
-        *  STEP (6): inform the user that they are done with sign-up!
+        *  Inform the user that they are done with onboarding
         **************************************************************/
         // use the `ORKCompletionStep` from ResearchKit
         let completionStep = ORKCompletionStep(identifier: "CompletionStep")
-        completionStep.title = "Completion Step Title"
-        completionStep.text = "Completion Step Text"
+        completionStep.title = "Onboarding Complete"
+        completionStep.text = "Onboarding is complete!"
         
         /* **************************************************************
-        * finally, CREATE an array with the steps to show the user
+        * Create an array with the steps to show the user
         **************************************************************/
         
         // given intro steps that the user should review and consent to
-        let introSteps: [ORKStep] = [consentStep, reviewConsentStep]
+        let stepsToUse: [ORKStep] = [consentStep, reviewConsentStep, completionStep]
         
-        // guide the user through ALL steps
-        let fullSteps = introSteps
-        
-        // unless they have already gotten as far as to enter an email address
-        var stepsToUse = fullSteps
         
         /* **************************************************************
-        * and SHOW the user these steps!
+        * Show the user these steps
         **************************************************************/
         // create a task with each step
         let orderedTask = ORKOrderedTask(identifier: "StudyOnboardingTask", steps: stepsToUse)
         
         // wrap that task on a view controller
         let taskViewController = ORKTaskViewController(task: orderedTask, taskRun: nil)
-        taskViewController.delegate = context.coordinator // enables `ORKTaskViewControllerDelegate` below
         
         // & present the VC!
         return taskViewController
