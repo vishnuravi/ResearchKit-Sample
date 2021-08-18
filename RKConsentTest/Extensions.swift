630c3eed1952a1fb64f0ee5b2d87c73b8bb9236b
Santiago Gutierrez <ssgutierrez42@gmail.com>
2021-08-18 13:45:22 -0700

    Launch RK via UIKit

---
+++ RKConsentTest/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  RKConsentTest
+//
+//  Created by Santiago Gutierrez on 8/18/21.
+//
+
+import Foundation
---
+++ RKConsentTest/OnboardingViewControllerUIKit.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingViewController_UIKit.swift
+//  RKConsentTest
+//
+//  Created by Santiago Gutierrez on 8/18/21.
+//
+
+import UIKit
+import ResearchKit
+
+class OnboardingViewControllerUIKit: ORKTaskViewController {
+    
+    
+    
+}
